b74ee2a8ddc8a5a56e3b8b49e7d04075026c568d
Luís Monteiro <monteiro.lcm@gmail.com>
2022-02-14 23:51:09 +0000

    Improvement

--- include/static.hpp
+++ include/static.hpp
@@ -106,25 +106,26 @@ namespace stc {
   struct identity {
     using type = T;
   };
+  template<int n>
+  struct number {
+    const int value{n};
+  };
   template<typename...>
   struct checker: identity<int>{};
 
 // =============================================================================
 // static helpers
 // ============================================================================= 
-  namespace details {
-    template<typename T>
-    struct process {
-      template<typename F>
-      using type = function<T,F>;
-      template<typename F>
-      static auto make(F&& fn) { 
-        return type<F>{std::forward<F>(fn)};
-      }   
+  namespace pipe {
+    template<typename T, typename F>  
+    struct part: function<T,F>{
+      using function<T,F>::function;
     };
-    struct step:process<step>{};
-    struct close:process<close>{};
-    
+    template<typename T, typename F>
+    auto make(F&& fn) { 
+      return part<T,F>{std::forward<F>(fn)};
+    } 
+                                  
     template<typename T>
     using check = typename checker<
       decltype(std::declval<T>().cbegin()),
@@ -135,6 +136,7 @@ namespace stc {
     constexpr auto declval()->T{ return {}; }
     template <>
     constexpr auto declval()->void{}
+    constexpr auto reflect = [](auto v){ return v; };
   }
 
 // =============================================================================
@@ -142,55 +144,134 @@ namespace stc {
 // ============================================================================= 
   template<typename T>
   constexpr auto load(T&& in) {
-    return stc::details::step::make([
-      &in
-    ](auto&& nxt) mutable {
-      for(auto& v : in)
-        nxt(v);
-      return stc::details::declval<
-        decltype(nxt(in.front()))>();
+    return stc::pipe::make<stc::number<0>>(
+      [&in](auto&& nxt) mutable {
+        std::for_each(std::cbegin(in), std::cend(in), [&nxt](auto& v){ nxt(v); });
+        return pipe::declval<decltype(nxt(*std::cbegin(in)))>();
+      });
+  }
+  
+  template<typename T>
+  constexpr auto range(T beg, T end, T step) {
+    return stc::pipe::make<stc::number<0>>(
+      [=](auto&& nxt) mutable {
+        for(; beg<end; beg+=step)
+          nxt(beg);
+        return pipe::declval<decltype(nxt(beg))>();
+      });
+  }
+  
+  template<typename T , stc::pipe::check<T> = {}>
+  constexpr auto zip(const T& in) {
+    return stc::pipe::make<stc::number<1>>(
+      [&in](auto&& nxt) mutable {
+      return [
+        cur=std::cbegin(in),
+        end=std::cend(in),
+        nxt=std::move(nxt)
+      ](auto&& val) mutable { 
+        if(cur!=end)
+          nxt(std::make_tuple(val, *cur++)); 
+        return pipe::declval<decltype(
+          nxt(std::make_tuple(val,*cur)))>();
+      }; 
     });
   }
   
-  template<typename F>
-  constexpr auto transform(F fnc) {
-    return stc::details::step::make([
-      fnc=std::move(fnc)
-    ](auto&& nxt) mutable {
+  template<typename T = std::size_t>
+  constexpr auto enumerate(T init = {}) {
+    return stc::pipe::make<stc::number<1>>(
+      [init](auto&& nxt) mutable {
       return [
-        fnc=std::move(fnc),
+        cur=std::move(init),
         nxt=std::move(nxt)
       ](auto&& val) mutable { 
-        nxt(fnc(val)); 
-        return stc::details::declval<
-          decltype(nxt(fnc(val)))>();
+        nxt(std::make_tuple(cur++,val)); 
+        return pipe::declval<decltype(
+          nxt(std::make_tuple(cur,val)))>();
       }; 
     });
   }
   
   template<typename F>
+  constexpr auto transform(F fnc) {
+    return stc::pipe::make<stc::number<1>>(
+      [fnc=std::move(fnc)](auto&& nxt) mutable {
+        return [
+          fnc=std::move(fnc), 
+          nxt=std::move(nxt)
+        ](auto&& val) mutable { 
+          nxt(fnc(val)); 
+          return pipe::declval<decltype(nxt(fnc(val)))>();
+        }; 
+      });
+  }
+    
+  template<typename F>
+  constexpr auto transform_scan(F fnc) {
+    return stc::pipe::make<stc::number<2>>(
+      [fnc=std::move(fnc)](auto&& beg, auto&& nxt) mutable {
+        return [
+          fnc=std::move(fnc), 
+          nxt=std::move(nxt), 
+          acc=decltype(beg(pipe::reflect)){}
+        ](auto&& val) mutable {
+          acc = fnc(acc, val);
+          nxt(acc);
+          return stc::pipe::declval<decltype(nxt(acc))>();
+        };
+      });
+  }
+  
+  template<typename F>
+  constexpr auto transform_adjacent(F fnc) {
+    return stc::pipe::make<stc::number<2>>(
+      [fnc=std::move(fnc)](auto&& beg, auto&& nxt) mutable {
+        return [
+          fnc=std::move(fnc),
+          nxt=std::move(nxt),
+          acc=decltype(beg(pipe::reflect)){}
+        ](auto&& val) mutable {
+          nxt(fnc(val, acc));
+          acc = std::move(val);
+          return pipe::declval<decltype(nxt(fnc(acc,val)))>();
+        };
+      });
+  }
+  
+  template<typename F>
   constexpr auto filter(F fnc){
-    return stc::details::step::make([
-      fnc=std::move(fnc)
-    ](auto&& nxt) mutable {
-      return [
-        fnc=std::move(fnc),
-        nxt=std::move(nxt)
-      ](auto&& val) mutable { 
-        if(fnc(val)) nxt(val); 
-        return stc::details::declval<
-          decltype(nxt(val))>();
-      };
-    });
+    return stc::pipe::make<stc::number<1>>(
+      [fnc=std::move(fnc)](auto&& nxt) mutable {
+        return [
+          fnc=std::move(fnc),
+          nxt=std::move(nxt)
+        ](auto&& val) mutable { 
+          if(fnc(val)) nxt(val); 
+          return pipe::declval<decltype(nxt(val))>();
+        };
+      });
+  }
+  
+  template<std::size_t N>
+  constexpr auto head(){
+    return stc::pipe::make<stc::number<9>>(
+      [](auto&& beg) mutable {
+        auto res = stc::vector<decltype(beg(pipe::reflect)),N+1>{};
+        auto end = [&res](auto&& v){ res.push_back(v); };
+        beg(end);
+        if(res.max_size()<res.size())
+          res.pop_back();
+        return res;
+      });
   }
   
   template<std::size_t N, typename F>
-  constexpr auto sort(F&& fnc){
-    return stc::details::close::make([
+  constexpr auto top(F&& fnc){
+    return stc::pipe::make<stc::number<9>>([
       fnc = std::move(fnc)
     ](auto&& beg) mutable {
-      auto aux = [](auto a){ return a; };
-      auto res = stc::vector<decltype(beg(aux)),N+1>{};
+      auto res = stc::vector<decltype(beg(pipe::reflect)),N+1>{};
       auto end = [&res, &fnc](auto&& v) {
         res.push_back(v);
         std::rotate(
@@ -204,31 +285,63 @@ namespace stc {
       return res;
     });
   }
+  
+  template<typename F>
+  constexpr auto reduce(F&& fnc){
+    return stc::pipe::make<stc::number<9>>([
+      fnc = std::move(fnc)
+    ](auto&& beg) mutable {
+      auto res = decltype(beg(pipe::reflect)){};
+      auto end = [&res, &fnc](auto&& val) { res = fnc(res,val); };
+      beg(end);
+      return res;
+    });
+  }
+  
+  template<typename F>
+  constexpr auto attach(F&& fnc){
+    return stc::pipe::make<stc::number<9>>([
+      fnc = std::move(fnc)
+    ](auto&& beg) mutable {
+       return fnc(std::move(beg));
+    });                    
+  }
 }
 
 // =============================================================================
 // pipeline links
 // ============================================================================= 
-template<typename A, typename B, stc::details::check<A> = 0>
+template<typename A, typename B, typename C, stc::pipe::check<A> = {}>
 constexpr auto operator|(
-  A&& a,
-  stc::details::step::type<B>&& b) {
+  A&& a, stc::pipe::part<B,C>&& b) {
   return stc::load(std::forward<A>(a)) | std::move(b);
 }
+
 template<typename A,typename B>
 constexpr auto operator|(
-  stc::details::step::type<A>&& a,
-  stc::details::step::type<B>&& b) {
-  return stc::details::step::make([
-    a = std::move(a),
-    b = std::move(b)
-  ](auto&& c) mutable { 
-    return a(b(c)); 
+  stc::pipe::part<stc::number<0>,A>&& a, 
+  stc::pipe::part<stc::number<1>,B>&& b) {
+  return stc::pipe::make<stc::number<0>>([
+    a=std::move(a), 
+    b=std::move(b)](auto&& c) mutable { 
+      return a(b(c)); 
   });
 }
+
 template<typename A,typename B>
 constexpr auto operator|(
-  stc::details::step::type<A>&& a,
-  stc::details::close::type<B>&& b) {
-  return b(a);
+  stc::pipe::part<stc::number<0>,A>&& a, 
+  stc::pipe::part<stc::number<2>,B>&& b) {
+  return stc::pipe::make<stc::number<0>>([
+    a=std::move(a),
+    b=std::move(b)](auto&& c) mutable { 
+      return a(b(a,c)); 
+  });
+}
+
+template<typename A,typename B>
+constexpr auto operator|(
+  stc::pipe::part<stc::number<0>,A>&& a, 
+  stc::pipe::part<stc::number<9>,B>&& b) {
+  return b(std::move(a));
 }
